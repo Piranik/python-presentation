791192151102cc92f3a94299343daa59e8dd62ed
Razvan Chitu <razvan.ch95@gmail.com>
2017-04-01 01:09:05 +0300

    apple: add script with todos

---
+++ apple/pc_garage.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+# TODO 1: import the function for sending emails
+
+
+if __name__ == '__main__':
+    url = 'http://www.pcgarage.ro/smartphone/'
+
+    # TODO 2: execute GET request on url
+
+    # TODO 3: create a BeautifulSoup object based on the text from the request
+
+    # TODO 4: initialize phones list
+
+    # TODO 5: iterate over smartphone HTML elements
+    # Hint: look for divs with a class that has 'box' in its name
+
+        # TODO 6: get name of smartphone
+
+        # TODO 7: get price of smartphone
+
+        # TODO 8: create phone dictionary with name and price
+
+        # TODO 9: append phone to the list
+
+    # TODO 10: send the email
